866fde4963e48258b272326a43ff86734741476e
codinginpain <codinginpain@gmail.com>
2023-03-25 23:07:04 +0900

    ✨feature: auto siwte frame to to show pictures

---
+++ android/app/src/main/kotlin/com/example/flutter_auto_swipe_frame/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_auto_swipe_frame
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auto_swipe_frame/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
